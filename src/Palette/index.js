1aa1fb6a007dab054811bc7563fe4c6cfa391eb4
FrancescaBambozzi <bambozzif@gmail.com>
2023-01-25 00:20:59 +0100

    first commit

---
+++ src/Palette/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PaletteColor from "./PaletteColor.js";
+
+const Palette = (props) => {
+  const palette = props.palette.map((color) => (
+    <PaletteColor
+      key={color}
+      color={color}
+      removeFromPalette={props.removeFromPalette}
+    />
+  ));
+  return (
+    <div>
+      <h1>Color Palette</h1>
+      <h2>Tap over the colors to generate your own color palette!</h2>
+      <ul className="palette-colors">{palette}</ul>
+    </div>
+  );
+};
+
+export default Palette;
