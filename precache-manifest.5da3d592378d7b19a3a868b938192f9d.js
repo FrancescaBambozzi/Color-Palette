4c2ad9e2553e62149780786c8e7bc33af66a1920
FrancescaBambozzi <bambozzif@gmail.com>
2023-01-25 01:10:11 +0100

    Updates

---
+++ precache-manifest.5da3d592378d7b19a3a868b938192f9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f82a7591a4c8ee05c1c",
+    "url": "/Color-Palette/static/css/main.ff72ebc6.chunk.css"
+  },
+  {
+    "revision": "7f82a7591a4c8ee05c1c",
+    "url": "/Color-Palette/static/js/main.3a1ac23f.chunk.js"
+  },
+  {
+    "revision": "576c53424ba2a0cb3703",
+    "url": "/Color-Palette/static/js/runtime~main.21ea41b6.js"
+  },
+  {
+    "revision": "1997075cad3a890a23a6",
+    "url": "/Color-Palette/static/js/2.0b9f17f9.chunk.js"
+  },
+  {
+    "revision": "c731d143c681834652595403aab8c05c",
+    "url": "/Color-Palette/static/media/colors-unsplash.c731d143.jpg"
+  },
+  {
+    "revision": "621735a458fd2b66b1bd04d1174c59e6",
+    "url": "/Color-Palette/index.html"
+  }
+];
