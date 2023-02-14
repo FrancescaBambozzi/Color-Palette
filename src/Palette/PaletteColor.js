d46d1f4e6aad3e76b7a0fc97dbbaa8e87f9f5807
FrancescaBambozzi <bambozzif@gmail.com>
2023-02-14 15:37:39 +0100

    commit

--- src/Palette/PaletteColor.js
+++ src/Palette/PaletteColor.js
@@ -3,10 +3,6 @@ import React from "react";
 const PaletteColor = (props) => (
   <li
     className="palette-color"
-    onClick={function () {
-      {/* on click display the color name */}
-      var colorName = document.getElementsByClassName("color-name");
-    }}
   >
     <span
       className="palette-color-preview"
